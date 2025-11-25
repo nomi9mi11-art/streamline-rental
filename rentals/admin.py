d07b38ab16b8e8699c5e230d496ac4776f283190
nomi9mi11-art <nomi9mi11@gmail.com>
2025-11-25 23:14:04 +0500

    Added venv-based requirements.txt

--- rentals/admin.py
+++ rentals/admin.py
@@ -1,11 +1,38 @@
 from django.contrib import admin
-
-# Register your models here.
-#from django.contrib import admin
 from .models import CarCategory, Car, Customer, Booking, Payment
 
-admin.site.register(CarCategory)
-admin.site.register(Car)
-admin.site.register(Customer)
-admin.site.register(Booking)
-admin.site.register(Payment)
+
+@admin.register(CarCategory)
+class CarCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['name', 'brand', 'model_year', 'rent_per_day', 'is_available', 'category']
+    list_filter = ['is_available', 'category', 'brand']
+    search_fields = ['name', 'brand']
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'phone', 'email', 'cnic']
+    search_fields = ['full_name', 'cnic', 'phone']
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['car', 'customer', 'start_date', 'end_date', 'total_amount', 'status']
+    list_filter = ['status', 'start_date']
+    search_fields = ['car__name', 'customer__full_name']
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['booking', 'amount_paid', 'payment_date']
+    readonly_fields = ['payment_date']
+
+admin.site.site_header = "Streamline Rental Admin"
+admin.site.site_title = "Streamline Rental"
+admin.site.index_title = "Welcome to Streamline Rental Dashboard"
