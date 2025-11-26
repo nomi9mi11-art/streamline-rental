bbede34c4fb5b611443e1b2a74ca8abe8b8c554b
nomi9mi11-art <nomi9mi11@gmail.com>
2025-11-27 00:20:18 +0500

    Added frontend views and URLs

--- rentals/urls.py
+++ rentals/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
 from . import views
-from django.urls import path, include
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -10,6 +7,4 @@ urlpatterns = [
     path("cars/<int:car_id>/", views.car_detail, name="car_detail"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
-    path("", include("rentals.urls")),
-    path('admin/', admin.site.urls),
 ]
