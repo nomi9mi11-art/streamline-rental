c394639495e8ee36fcfa7a727932aaf5fa608f02
nomi9mi11-art <nomi9mi11@gmail.com>
2025-11-26 21:07:36 +0500

    Updated create_admin.py final version

--- create_admin.py
+++ create_admin.py
@@ -1,3 +1,9 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+django.setup()
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
